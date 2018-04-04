e300da9a74f32c2b1960b6bf5dd87aaf03126c3c
jmank88 <jmank88@gmail.com>
2018-04-04 11:55:29 -0500

    rename to gochain

--- src-lite/js/filters.js
+++ src-lite/js/filters.js
@@ -78,13 +78,6 @@ angular.module('netStatsApp.filters', [])
 		{
 			var tmp = version.split('/');
 
-			tmp[0] = tmp[0].replace('Ethereum(++)', 'Eth');
-
-			if(tmp[0].indexOf('pyethapp') === 0)
-			{
-				tmp[0] = 'pyeth';
-			}
-
 			if(tmp[1][0] !== 'v' && tmp[1][2] !== '.')
 			{
 				tmp.splice(1,1);
--- web-app/.meteor/local/build/programs/web.browser/app/client/js/templateHelpers.js
+++ web-app/.meteor/local/build/programs/web.browser/app/client/js/templateHelpers.js
@@ -594,13 +594,6 @@ Template.registerHelper('nodeVersion', function(version) {
     {
         var tmp = version.split('/');
 
-        tmp[0] = tmp[0].replace('Ethereum(++)', 'Eth');
-
-        if(tmp[0].indexOf('pyethapp') === 0)
-        {
-            tmp[0] = 'pyeth';
-        }
-
         if(tmp[1][0] !== 'v' && tmp[1][2] !== '.')
         {
             tmp.splice(1,1);
