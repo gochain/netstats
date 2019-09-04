f6a63b1067e88e98f7ef88efe20a9bf8821dc641
jmank88 <jmank88@gmail.com>
2019-09-03 20:03:01 -0500

    fix CI

--- release.sh
+++ release.sh
@@ -19,7 +19,7 @@ version_file="package.json"
 docker create -v /data --name file alpine /bin/true
 docker cp ${HOME}/project/$version_file file:/data/$version_file
 # Bump version, patch by default - also checks if previous commit message contains `[bump X]`, and if so, bumps the appropriate semver number - https://github.com/treeder/dockers/tree/master/bump
-docker run --rm -it --volumes-from file -w /app treeder/bump --filename $version_file "$(git log -1 --pretty=%B)"
+docker run --rm -it --volumes-from file -w /app treeder/bump --filename /data/$version_file "$(git log -1 --pretty=%B)"
 version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
 echo "Version: $version"
 
